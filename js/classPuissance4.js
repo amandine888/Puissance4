e4b5158e4001d346a1f5ee816be1fde7488a04cb
Amandine <amandine2.divay@gmail.com>
2019-11-02 13:22:47 +0100

    add méthode check Horizontal

--- js/classPuissance4.js
+++ js/classPuissance4.js
@@ -48,7 +48,7 @@ class Puissance4 {
 
         this.turn = (this.turn === this.player1) ? this.player2 : this.player1;
 
-        this.check_col(y);
+        this.check_win(x, y, this.player1);
 
     }
 
@@ -62,9 +62,9 @@ class Puissance4 {
     
     add_token(y,x) {
         if(this.turn === this.player1){
-        $("tr:nth-child(" + (y+1) + ") td:nth-child(" + (x+1) + ") img").attr("src", "./img/Jeton.png")}
+        $("tr:nth-child(" + (y+1) + ") td:nth-child(" + (x+1) + ") img").attr("src", "./img/Ada.png")}
             else {this.player2
-            $("tr:nth-child(" + (y+1) + ") td:nth-child(" + (x+1) + ") img").attr("src", "./img/jetonalien.png")}
+            $("tr:nth-child(" + (y+1) + ") td:nth-child(" + (x+1) + ") img").attr("src", "./img/Hedi.png")}
         this.count_turn++;
         console.log("countTurn",this.count_turn)
         if(this.count_turn === this.max_turn){
@@ -74,12 +74,44 @@ class Puissance4 {
         }
     }
 
-    check_col(y){
+    check_win(column, row, player1, player2) {
+
+        // Horizontal pour player 1 : 
+        let count = 0;
+        for (let j = 0; j < this.grid_dimension.x  ; j++) {
+            count = (this.grid[row][j] == this.player1) ? count + 1 : 0;
+            if (count >= 4){
+                window.alert("Player1 Win");
+            }
+        }
+
+        // Horizontal pour player 2 : 
+        let count2 = 0;
+        for (let j = 0; j < this.grid_dimension.x  ; j++) {
+            count2 = (this.grid[row][j] == this.player2) ? count2 + 1 : 0;
+            if (count2 >= 4){
+                window.alert("Player2 Win");
+            }
         
-    }
+        } 
+        
+        // Vertical pour player 1 : 
+        // let countVert = 0;
+        // for (let i = 0; i < this.grid_dimension.y  ; i++) {
+        //     countVert = (this.grid[column][i] == this.player1) ? countVert + 1 : 0;
+        //     if (countVert >= 4){
+        //         window.alert("Player1 Win");
+        //     }
+        // }
+      
+        // Diagonal
+        
+        // Anti-diagonal
+    } 
+    
 
 
-}
+
 
  //cibler et placer jeton sur img, mettre condition pour changer jeton en fonction du joueur. 
         //alterner joueur courant (this.turn)
@@ -88,3 +120,5 @@ class Puissance4 {
 
         //check
         //regarder si victoire ou pas ou null
+
+}
